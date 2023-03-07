969a79e04afbcd57372a9206375fb26635fcfb94
testsolo8 <96113640+testsolo8@users.noreply.github.com>
2023-03-07 16:46:24 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-from pprint import pprint
-
-BROWSER_OPTIONS = {
-    "geolocation": {"latitude": 48.8, "longitude": 2.3},
-    "permissions": ["geolocation"],
-}
-
-SECURE_OPTIONS = {"security.enterprise_roots.enabled": True}
